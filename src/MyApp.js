b7484f920d9b6da31fbf5ae6a4e9a5e7a54cfef5
polinavolnuhina <pvolnuhi@calpoly.edu>
2021-01-15 20:32:04 -0800

    Submitting Final React first app II

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,67 +1,63 @@
 import React, {useState} from 'react';
-import Table from './Table'
-
+import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-    const [characters, setCharacters] = useState([  
-       {
-         name: 'Charlie', 
-         job: 'Janitor',
-         // the rest of the data
-       },
-       {
-        name: 'Mac',
-        job: 'Bouncer',
-        },
-        {
-        name: 'Dee',
-        job: 'Aspring actress',
-        },
-        {
-        name: 'Dennis',
-        job: 'Bartender',
-        },
-    ]);
+  const [characters, setCharacters] = useState([]);
 
-function removeOneCharacter (index) {
+  function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
-        return i !== index
+      return i !== index
     });
     setCharacters(updated);
-    }
-    return (
-        <div className="container">
-          <Table characterData={characters} removeCharacter={removeOneCharacter} />
-        </div>
-    ) 
+  }
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+//    function updateList(person) { 
+//     makePostCall(person).then( result => {
+//     if (result.status === 201)
+//       setCharacters([...characters, result.data] );
+//     });
+//   }
+
+//   useEffect(() => {
+//     fetchAll().then( result => {
+//       if (result)
+//         setCharacters(result);
+//      });
+//   }, [] );
+
+//   async function fetchAll(){
+//     try {
+//       const response = await axios.get('http://localhost:5000/users');
+//       return response.data.users_list;     
+//     }
+//     catch (error){
+//       //We're not handling errors. Just logging into the console.
+//       console.log(error); 
+//       return false;         
+//     }
+//  }
+
+//   async function makePostCall(person){
+//     try {
+//       const response = await axios.post('http://localhost:5000/users', person);
+//       return response;
+//     }
+//     catch (error) {
+//       console.log(error);
+//       return false;
+//     }
+//   }
+  
+   return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  )
 }
-// const characters = [
-//     {
-//       name: 'Charlie',
-//       job: 'Janitor',
-//     },
-//     {
-//       name: 'Mac',
-//       job: 'Bouncer',
-//     },
-//     {
-//       name: 'Dee',
-//       job: 'Aspring actress',
-//     },
-//     {
-//       name: 'Dennis',
-//       job: 'Bartender',
-//     },
-// ];
-
-// function MyApp() {
-//     return (
-//         <div className="container">
-//           <Table characterData={characters} />
-//         </div>
-//     )
-// }
-
-
 
 export default MyApp;
